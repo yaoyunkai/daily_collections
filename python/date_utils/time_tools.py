469842b5a315981bff40e23aa66f4aa2846d250b
liberty <1052433260@qq.com>
2023-02-13 21:56:28 +0800

    insert into DB for test record

--- python/date_utils/time_tools.py
+++ python/date_utils/time_tools.py
@@ -8,3 +8,7 @@ def formatted_seconds(seconds):
     m, s = divmod(int(seconds), 60)
     h, m = divmod(m, 60)
     return '{}:{:02d}:{:02d}'.format(h, m, s)
+
+
+if __name__ == '__main__':
+    print(formatted_seconds(343244554))
