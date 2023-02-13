43dd88abd83db665ccd86042263825bba5c51215
liberty <1052433260@qq.com>
2023-02-13 23:25:20 +0800

    test record: analysis data

--- python/test_record/analysis_data.py
+++ python/test_record/analysis_data.py
@@ -1,9 +1,27 @@
 """
 分析数据
 
-First Pass Yield
-Test  Yield
-Board Yield
+yield type:
+    First Pass Yield
+    Test  Yield
+    Board Yield
+
+view type:
+    none
+    month
+    week
 
 
 """
+
+
+def _deal_with_params(params_dict: dict):
+    pass
+
+
+def get_yield(sernum='', uuttype='', area='', test_server='', from_date='', to_date='', yield_type='', view_type=''):
+    print(locals())
+
+
+if __name__ == '__main__':
+    get_yield()
