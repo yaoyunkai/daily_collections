2a38f2bfa21b78e134a20a9f2e8cc9c808b73bea
yunk <1052433260@qq.com>
2023-03-16 11:51:00 +0800

    update

--- python/pydantic_usage/simple1.py
+++ python/pydantic_usage/simple1.py
@@ -3,35 +3,31 @@
 
 Create at 2023/3/15 20:08
 """
+import pprint
 from datetime import datetime
 from typing import List, Optional
 
-from pydantic import BaseModel, ValidationError, compiled
+from pydantic import BaseModel
 
 
 class User(BaseModel):
     id: int
     name = 'John Doe'
     signup_ts: Optional[datetime] = None
-    friends: List[int] = []
+
+    friends: List[int]
 
 
 if __name__ == '__main__':
+    # print('complied: {}'.format(compiled))  # 验证是否是编译运行的
+
     external_data = {
         'id': '123',
+        'name': 'hhhh"{}',
         'signup_ts': '2019-06-01 12:22',
         'friends': [1, 2, '3'],
     }
     user = User(**external_data)
-    print('complied: {}'.format(compiled))  # 验证是否是编译运行的
     print(user.json())
-
-    # print(user.id)
-    # print(user.signup_ts)
-    # print(user.friends)
-    # print(user.dict())
-
-    try:
-        User(id=124, name='Tom', friends=[1, 2, 'xx'])
-    except ValidationError as e:
-        print(e.json())
+    print(user.dict())
+    pprint.pprint(user.schema())
