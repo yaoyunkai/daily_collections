d5f743fd0590154293837f679f2bdbc2c1eac287
yunk <1052433260@qq.com>
2023-07-11 17:27:28 +0800

    fastapi usage

---
+++ python/third_package/fastapi_usage/sql_app/schemas.py
@@ -0,0 +1,44 @@
+"""
+
+
+Created at 2023/7/11
+"""
+
+from typing import List
+from typing import Union
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: Union[str, None] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
