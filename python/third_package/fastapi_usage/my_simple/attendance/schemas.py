bd9b9d1c6e75ebbaf96242ab0766b3e8800a2213
yunk <1052433260@qq.com>
2023-07-17 14:48:00 +0800

    时间相关的问题

--- python/third_package/fastapi_usage/my_simple/attendance/schemas.py
+++ python/third_package/fastapi_usage/my_simple/attendance/schemas.py
@@ -4,6 +4,7 @@
 Created at 2023/7/15
 """
 import datetime
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -26,3 +27,16 @@ class AttendanceRecord(AttendanceRecordBase):
 
     class Config:
         orm_mode = True
+
+
+class SearchParams(BaseModel):
+    """
+    for search_records
+
+    """
+
+    staff_id: Optional[str]
+    record_type: Optional[str]
+
+    start_time: str
+    end_time: Optional[str]
--- python/third_package/fastapi_usage/my_simple/attendance/views.py
+++ python/third_package/fastapi_usage/my_simple/attendance/views.py
@@ -6,6 +6,7 @@ response_class用于自定义响应的格式和发送回客户端的方式。
 
 Created at 2023/7/15
 """
+
 from typing import List
 
 from fastapi import APIRouter
@@ -16,6 +17,7 @@ from sqlalchemy.orm import Session
 from . import crud
 from . import models
 from . import schemas
+from . import utils
 from ..database import get_db
 
 router = APIRouter(
@@ -33,11 +35,35 @@ async def create_record(record: schemas.AttendanceRecordBase, db: Session = Depe
 
 @router.get('/get/', response_model=List[schemas.AttendanceRecord])
 async def get_all_records(db: Session = Depends(get_db)):
-    records = db.query(models.AttendanceRecord).all()
+    records = db.query(models.AttendanceRecord). \
+        order_by(models.AttendanceRecord.record_time). \
+        all()
     return records
 
 
 @router.get('/get/{staff_id}/', response_model=List[schemas.AttendanceRecord])
 async def get_records_by_staff_id(staff_id: str, db: Session = Depends(get_db)):
-    records = db.query(models.AttendanceRecord).filter(models.AttendanceRecord.staff_id == staff_id).all()
+    records = db.query(models.AttendanceRecord). \
+        filter(models.AttendanceRecord.staff_id == staff_id). \
+        order_by(models.AttendanceRecord.record_time). \
+        all()
     return records
+
+
+@router.post('/search/', response_model=List[schemas.AttendanceRecord])
+async def search_records(params: schemas.SearchParams):
+    """
+    staff_id 可选
+
+    record_type : 可选, in / out , 不选就是 in and out
+
+    """
+    print(params)
+    if params.staff_id and not utils.is_valid_staff_id(params.staff_id):
+        raise HTTPException(status_code=400, detail='not valid staff id')
+
+    if params.record_type and not models.is_valid_record_type(params.record_type):
+        raise HTTPException(status_code=400,
+                            detail='record type is not valid, must be "in" or "out"')
+
+    return []
