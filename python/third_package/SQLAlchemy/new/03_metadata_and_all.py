f8907b1f521d5a58e99f8fb2eec15cbb79c35c6d
yunk <1052433260@qq.com>
2023-08-21 17:59:07 +0800

    SQL function in sqlalchemy

--- python/third_package/SQLAlchemy/new/03_metadata_and_all.py
+++ python/third_package/SQLAlchemy/new/03_metadata_and_all.py
@@ -17,11 +17,13 @@ from sqlalchemy import MetaData
 from sqlalchemy import and_, or_
 from sqlalchemy import bindparam
 from sqlalchemy import create_engine
-from sqlalchemy import func
+from sqlalchemy import desc, func
 from sqlalchemy import insert
 from sqlalchemy import literal_column
+from sqlalchemy.dialects import oracle, postgresql
 from sqlalchemy.orm import DeclarativeBase, Session
 from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import aliased
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 
@@ -296,6 +298,101 @@ def from_in_select_sql():
     print(select(user_table).join(address_table, full=True))
 
 
+def order_by_in_select():
+    print(select(user_table).order_by(user_table.c.name.asc()))
+    print(select(User).order_by(User.fullname.desc()))
+
+    stmt = func.count(user_table.c.id)
+    print(stmt)
+
+    with engine.connect() as conn:
+        result = conn.execute(
+            select(User.name, func.count(Address.id).label("count"))
+            .join(Address)
+            .group_by(User.name)
+            .having(func.count(Address.id) > 1)
+        )
+        print(result.all())
+
+    # group_by order_by 既可以传递字符串，也可以传递column实例
+    stmt = (
+        select(Address.user_id, func.count(Address.id).label("num_addresses"))
+        .group_by(Address.user_id)
+        .order_by("user_id", desc("num_addresses"))
+    )
+    print(stmt)
+
+    # 使用别名来引用同一个表
+    user_alias_1 = user_table.alias()
+    print(f'type is : {type(user_alias_1)}')
+
+    user_alias_2 = user_table.alias()
+    print(
+        select(user_alias_1.c.name, user_alias_2.c.name).join_from(
+            user_alias_1, user_alias_2, user_alias_1.c.id > user_alias_2.c.id
+        )
+    )
+
+    # ORM 使用方法 aliased
+    address_alias_1 = aliased(Address)
+    address_alias_2 = aliased(Address)
+    print(
+        select(User)
+        .join_from(User, address_alias_1)
+        .where(address_alias_1.email_address == "patrick@aol.com")
+        .join_from(User, address_alias_2)
+        .where(address_alias_2.email_address == "patrick@gmail.com")
+    )
+
+
+def scalar_subquery_sql():
+    subq = (
+        select(func.count(address_table.c.id))
+        .where(user_table.c.id == address_table.c.user_id)
+        .scalar_subquery()
+    )
+    print(subq)
+
+    print(subq == 5)
+
+    stmt = select(user_table.c.name, subq.label("address_count"))
+    print(stmt)
+
+    subq = (
+        select(func.count(address_table.c.id))
+        .where(user_table.c.id == address_table.c.user_id)
+        .group_by(address_table.c.user_id)
+        .having(func.count(address_table.c.id) > 1)
+    ).exists()
+    with engine.connect() as conn:
+        result = conn.execute(select(user_table.c.name).where(subq))
+        print(result.all())
+
+
+def sql_function():
+    print(select(func.count()).select_from(user_table))
+
+    print(select(func.lower("A String With Much UPPERCASE")))
+
+    with engine.connect() as conn:
+        result = conn.execute(select(func.lower("A String With Much UPPERCASE")))
+        print(result.all())
+
+    stmt = select(func.now())
+    with engine.connect() as conn:
+        result = conn.execute(stmt)
+        print(result.all())
+
+    # func 是一个class, 由于sql的方法很多 所以func支持任何传入的属性
+    print(select(func.some_crazy_function(user_table.c.name, 17)))
+
+    print(select(func.now()).compile(dialect=postgresql.dialect()))
+    print(select(func.now()).compile(dialect=oracle.dialect()))
+
+    #  "SQL 返回类型"，指的是函数在数据库侧 SQL 表达式中返回的 SQL 值类型，而不是 Python 函数的 "返回类型"。
+    print(func.now().type)
+
+
 if __name__ == '__main__':
     # print_columns()
     # check_metadata()
@@ -306,5 +403,6 @@ if __name__ == '__main__':
     # insert_sql_adv()
 
     # select_sql_metadata()
-
-    from_in_select_sql()
+    # from_in_select_sql()
+    # scalar_subquery_sql()
+    sql_function()
