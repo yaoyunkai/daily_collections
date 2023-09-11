de567c67869ffde6977b2fbbc6bb72a2eab0749a
yunk <1052433260@qq.com>
2023-09-11 17:28:47 +0800

    update

--- python/third_package/fastapi_usage/part1/1_start_use.py
+++ python/third_package/fastapi_usage/part1/1_start_use.py
@@ -35,3 +35,9 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app)
