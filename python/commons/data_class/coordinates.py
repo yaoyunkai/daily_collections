e860efa286dbb85c44931874bc2b14fd60b0bca2
yunk <1052433260@qq.com>
2024-09-25 11:46:59 +0800

    update

--- python/commons/data_class/coordinates.py
+++ python/commons/data_class/coordinates.py
@@ -4,6 +4,18 @@
 Created at 2023/4/8
 """
 
+import typing
+
+
+def usage_of_namedtuple():
+    Coordinate1 = typing.NamedTuple('Coordinate1', lat=float, lon=float)
+    print(typing.get_type_hints(Coordinate1))
+
+    pos0 = Coordinate1(12.34, 12.12)
+    print(pos0)
+
+    print(pos0 == Coordinate1(12.34, 12.12))
+
 
 class Coordinate:
     def __init__(self, lat, lon):
@@ -16,3 +28,5 @@ if __name__ == '__main__':
     pos2 = Coordinate(55.76, 37.62)
     print(pos1 == pos2)
     print(pos1 == Coordinate(55.76, 37.62))
+
+    usage_of_namedtuple()
