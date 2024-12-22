b9d1578032512b515e2ac97b30ce688ce3483084
yunk <1052433260@qq.com>
2024-12-22 21:50:28 +0800

    update

---
+++ python/demos/2024-12/sampling_algm/utils.py
@@ -0,0 +1,26 @@
+"""
+抽样算法
+    by uuttype
+    by group
+
+
+Item:
+    id
+    record_time
+    test_id
+    sernum
+    uuttype
+    area
+    test_failure
+    passfail
+
+
+"""
+
+
+def add_to_pool(test_record_item):
+    """
+    将数据添加到抽样池中
+
+    """
+    pass
