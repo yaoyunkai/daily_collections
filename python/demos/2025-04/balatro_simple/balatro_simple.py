457edff5846b123af50d8786c2db4969311a096a
yunk <1052433260@qq.com>
2025-04-22 22:56:30 +0800

    update balatro

--- python/demos/2025-04/balatro_simple/balatro_simple.py
+++ python/demos/2025-04/balatro_simple/balatro_simple.py
@@ -18,17 +18,32 @@
 同花五条
 
 =============================================
+哪些小丑牌会影响牌型计算呢？
+
+    增强牌组: 石头牌，万能牌
+    小丑:
+
+
 四指: 同花和顺子可以由四张牌构成
 幻视: 所有卡牌视为人头牌。 和 学者 互动吗？
 学者: 打出的A在计分时加4倍和20筹码
 捷径: 让顺子可以相隔一个点数组成。优先级不如对子？
 模糊小丑: 红桃和方块视为一个花色 黑桃和梅花视为一个花色。
+哑剧演员: 重新触发留在手牌中的牌的能力
+飞溅: 每张打出的牌都可以计分。
+吸血鬼: 每打出一张计分的增加牌组，这张小丑牌获得0.1 倍率，并移除卡牌的增强效果。
+喜与悲: 重新触发所有打出的人头牌。
 
 
 万能牌  wild card
 石头牌  stone card  不影响牌型计算。
 
 最多打出五张牌
+
+
+on_start ->
+on_compute ->
+
 
 created at 2025/4/17
 """
@@ -54,7 +69,23 @@ CHIP_MAPPING = {
     '2': 2,
 }
 
-_unset = object()
+
+class DeckType(StrEnum):
+    RedDeck = 'red_deck'  # 加一弃牌次数
+    BlueDeck = 'blue_deck'  # 加一出牌次数
+    YellowDeck = 'yellow_deck'  # 开局获得额外的10块
+    GreenDeck = 'green_deck'  # Round 回合结束时，每个剩余出牌 2块钱， 每个剩余弃牌 1块钱，不赚取任何利息
+    BlackDeck = 'black_deck'
+    MagicDeck = 'magic_deck'
+    NebulaDeck = 'nebula_deck'
+    GhostDeck = 'ghost_deck'
+    AbandonedDeck = 'abandoned_deck'
+    CheckeredDeck = 'checkered_deck'
+    ZodiacDeck = 'zodiac_deck'
+    PaintedDeck = 'painted_deck'
+    AnaglyphDeck = 'anaglyph_deck'
+    PlasmaDeck = 'plasma_deck'
+    ErraticDeck = 'erratic_deck'
 
 
 class SuitType(StrEnum):
@@ -75,7 +106,7 @@ class EnhancedType(StrEnum):
 
 class Card:
     """
-
+    额外的位置记录 筹码
 
     suit:
         Spade    黑桃
@@ -112,12 +143,37 @@ class Card:
 
 
 class Application:
+    # 拥有某个小丑的标志位
+    flag_joker_smeared_joker = False  # 模糊小丑
+    flag_joker_four_fingers = False  # 四指
+    flag_joker_shortcut = False  # 捷径
+    flag_joker_splash = False  # 飞溅
+    flag_joker_pareidolia = False  # 幻视
+
     def __init__(self):
+        self.deck_type = None  # 牌组类型, 可能对计分方式有影响
         self.jokers = []
-        self.joker_slot = 5
+        self.joker_slot = 5  # 当前总共的小丑栏位
         self.current_play_cards = []
 
+    def _compute_joker_flags(self):
+        for attr_name in self.__class__.__dict__:
+            if attr_name.startswith('flag_joker_'):
+                setattr(self, attr_name, False)
+
+    def set_deck_type(self, deck_type: DeckType):
+        if self.deck_type:
+            print(f'already set deck type as {self.deck_type}, can\'t set again')
+            return
+
+        self.deck_type = deck_type
+        if self.deck_type is DeckType.BlackDeck:
+            self.joker_slot = 6
+
     def compute_poker_hands(self, played_cards: list[Card]):
+        if not self.deck_type:
+            raise SystemError('can\'t play without deck type')
+
         print(f'choices {_print_noun("card", len(played_cards))} for start play')
         self.current_play_cards = played_cards
 
@@ -128,6 +184,9 @@ class Application:
         """
         if len(self.current_play_cards) < 5:
             return False
+
+        first = self.current_play_cards[0]
+
         return None
 
 
@@ -136,4 +195,5 @@ if __name__ == '__main__':
     c2 = Card('K', SuitType.Diamond)
 
     app = Application()
+    app.set_deck_type(DeckType.PlasmaDeck)
     app.compute_poker_hands([c1, c2])
