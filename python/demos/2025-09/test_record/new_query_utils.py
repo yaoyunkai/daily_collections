dd48b41b5b56dd155d3c1fdf0a040e10f9cb60de
yunk <1052433260@qq.com>
2026-04-16 16:48:44 +0800

    update

--- python/demos/2025-09/test_record/new_query_utils.py
+++ python/demos/2025-09/test_record/new_query_utils.py
@@ -8,11 +8,46 @@ get_test_record_by_sernum
 created at 2026-04-15
 """
 
+from datetime import date
+from enum import Enum
+from typing import Optional
+
 from data_utils import ENGINE
+from pydantic import BaseModel
 from QueryExpr.runner import parse_query
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from test_record import TestRecord
+
+MAX_LIMIT = 5000
+
+
+class DataType(Enum):
+    TEST = "test"
+    FIRST_PASS = "first_pass"
+
+
+class ViewType(Enum):
+    NO_DATE = "no_date"
+    WEEK = "week"
+    MONTH = "month"
+
+
+class MultiSearchParams(BaseModel):
+    sernum: Optional[str] = None
+    uuttype: Optional[str] = None
+    test_machine: Optional[str] = None
+    test_area: Optional[str] = None
+
+    start_date: date
+    end_date: date
+
+    data_type: DataType = DataType.TEST
+
+    # must select one of below include_
+    include_pass: bool = True
+    include_fail: bool = True
+    include_start: bool = True
 
 
 class Querying(object):
